410b40074d9ac7ad1e576e9da874ed2faa01a40b
Leon <notLe0n75@gmail.com>
2021-08-17 14:49:31 +0200

    added Horizontal Scrollbar to ILWindow and fixed overlap, you can now invoke parameterless methods

--- src/Tools/AssemblyViewer/ILWindow.cs
+++ src/Tools/AssemblyViewer/ILWindow.cs
@@ -16,26 +16,49 @@ namespace Creativetools.src.Tools.AssemblyViewer
         {
             var instructions = Disassembler.GetInstructions(method);
 
-            var panel = new DragableUIPanel(method.Name, 400, 500);
+            var panel = new DragableUIPanel(method.Name, 400, 550);
             panel.Top.Set(200, 0);
             panel.Left.Set(500, 0);
             panel.OnCloseBtnClicked += () => UISystem.UserInterface2.SetState(null);
             Append(panel);
 
+            if (method.GetParameters().Length == 0)
+            {
+                var invokeButton = new UITextPanel<string>("invoke", 0.8f);
+                invokeButton.SetPadding(5);
+                invokeButton.Top.Set(31, 0);
+                invokeButton.Left.Set(10, 0);
+                invokeButton.Width.Set(100, 0);
+                invokeButton.Height.Set(10, 0);
+                invokeButton.OnClick += (evt, elm) =>
+                {
+                    method.Invoke(null, null);
+                    Terraria.Main.NewText($"<AssemblyViewer> Invoked method [c/FF0000:{method.Name}()] successfully!");
+                };
+                panel.Append(invokeButton);
+            }
+
             var scrollbar = new UIScrollbar();
             scrollbar.Top.Set(0, 0.1f);
-            scrollbar.Left.Set(0, 0.95f);
+            scrollbar.Left.Set(0, 0.90f);
             scrollbar.Width.Set(20, 0);
             scrollbar.Height.Set(0, 0.8f);
             panel.Append(scrollbar);
 
+            var horizontalScrollbar = new UIHorizontalScrollbar();
+            horizontalScrollbar.Top.Set(0, 0.925f);
+            horizontalScrollbar.Left.Set(10, 0f);
+            horizontalScrollbar.Width.Set(0, 0.8f);
+            panel.Append(horizontalScrollbar);
+
             var list = new BetterUIList();
             list.Top.Set(0, 0.1f);
             list.Left.Set(10, 0);
-            list.Width.Set(0, 0.95f);
-            list.Height.Set(0, 0.85f);
+            list.Width.Set(0, 0.8f);
+            list.Height.Set(0, 0.8f);
             list.ListPadding = 5;
             list.SetScrollbar(scrollbar);
+            list.SetHorizontalScrollbar(horizontalScrollbar);
             panel.Append(list);
 
             for (int i = 0; i < instructions.Count; i++)
